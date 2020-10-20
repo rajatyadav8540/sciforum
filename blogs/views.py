1692b56acdf92bafe630ea952de03354ba1c04e5
Rajat Yadav <rajatyadav26767@gmail.com>
2020-10-20 22:49:58 +0530

    commit after aading blog and event pages-Rajat

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request,'home/index.html')
+def bloglist(request):
+    return render(request,'blogs/bloglist.html')
