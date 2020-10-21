069558ee55a9934556f53984bb62b35b6cddd46f
Rajat Yadav <rajatyadav26767@gmail.com>
2020-10-22 00:36:41 +0530

    after adding postgres

--- events/models.py
+++ events/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class event(models.Model):
+
+    event_name= models.CharField(max_length=50)
+    event_content= models.CharField(max_length=1000)
