42b0704e840ed7f55e94376eee39f5d717153afe
Rajat Yadav <rajatyadav26767@gmail.com>
2020-10-25 12:48:46 +0530

    commit for check

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ class TeamProfile(models.Model):
     Profile_Post=models.CharField(max_length=50,default="member")
     Profile_content=models.CharField(max_length=5000,default="")
     Profile_email=models.CharField(max_length=50,default="scienceforum")
-    Profile_thumbnail=models.ImageField(upload_to="media\img\profiles",default="media\img\blogimg\science-1182713_1920.jpg")
+    Profile_thumbnail=models.ImageField(upload_to="img/profiles",default="img/blogimg/science-1182713_1920.jpg")
 
     def __str__(self):
         return self.Profile_title
