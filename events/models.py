74fe64aed7a21df0c65fc63f5190007edf4317e8
Rajat Yadav <rajatyadav26767@gmail.com>
2020-11-12 09:20:01 +0530

    fhgdf

--- events/models.py
+++ events/models.py
@@ -10,9 +10,9 @@ class event(models.Model):
     venue=models.CharField(max_length=50,default="",null=True)
     event_description=models.TextField(max_length=200,default="")
     event_content= RichTextField(max_length=2000,default="")
-    event_image1=models.ImageField(upload_to='img/eventimages',default="/static/img/logo.png")
-    event_image2=models.ImageField(upload_to='img/eventimages',default="/static/img/logo.png")
-    event_image3=models.ImageField(upload_to='img/eventimages',default="/static/img/logo.png")
+    event_image1=models.ImageField(upload_to='img/eventimages',default="img/logo.png")
+    event_image2=models.ImageField(upload_to='img/eventimages',default="img/logo.png")
+    event_image3=models.ImageField(upload_to='img/eventimages',default="img/logo.png")
 
     def __str__(self):
         return self.event_name
